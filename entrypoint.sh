c9af3f9c05bff30080b854227a21b25aee65216a
olblak <olivier@vernin.me>
2017-01-02 07:23:34 +0100

    Fix kubernetes config file path

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,8 @@
 : "${AZURE_SHARED_KEY?'Require AZURE_SHARED_KEY'}"
 
 # Update fluentd configuration file
-sed -i "s/CUSTOMER_ID/${AZURE_WORKSPACE_ID}/g" "/fluentd/etc/${FLUENTD_CONF}"
+sed -i "s/CUSTOMER_ID/${AZURE_WORKSPACE_ID}/g" "/fluentd/etc/conf.d/kubernetes.conf"
     # Use '#' instead of '/' as '/' is used inside ${AZURE_SHARED_KEY}
-sed -i "s#KEY_STRING#${AZURE_SHARED_KEY}#g" "/fluentd/etc/${FLUENTD_CONF}"
+sed -i "s#KEY_STRING#${AZURE_SHARED_KEY}#g" "/fluentd/etc/conf.d/kubernetes.conf"
 
 exec fluentd -c /fluentd/etc/${FLUENTD_CONF} -p /fluentd/plugins ${FLUENTD_OPT}
